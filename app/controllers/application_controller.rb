7cff40b22d3b2666315dad6e3b99282fb55ee9dc
Kate Raskauskas <katherine.raskauskas@gmail.com>
2020-01-29 14:21:10 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,46 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    article = Article.new({title:params[:title],content:params[:content]})
+    article.save
+    @id = article.id
+    redirect "/articles/#{@id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.all.find { |a| a.id == params[:id].to_i }
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do  #load edit form
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+ 
+patch '/articles/:id' do #edit action
+  @article = Article.find_by_id(params[:id])
+  @article.title = params[:title]
+  @article.content = params[:content]
+  @article.save
+  redirect to "/articles/#{@article.id}"
+end
+
+    delete '/articles/:id' do
+      @article = Article.find_by_id(params[:id])
+      @article.delete
+      redirect to '/articles'
+    end
 end
